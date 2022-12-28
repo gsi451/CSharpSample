71d418d19d3b80151cecdd5f3a2fb6b26f521311
gsi451 <gsi4517091@gmail.com>
2022-12-29 00:21:10 +0900

    해당 모니터의 상단의 가로크기에 맞는 큰 시계를 표시합니다. 표시내용 : (요일) 년-월-일 시:분:초:밀리초

---
+++ BigClock/BigClock/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BigClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.label1.Size = new System.Drawing.Size(835, 284);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "크기조정";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(77, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(782, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "종료";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(105)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(859, 319);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "GSI 시간";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BigClock/BigClock/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string GetWeek(DateTime nowDt)
+        {
+            string ret = "";
+            if (nowDt.DayOfWeek == DayOfWeek.Monday)
+                ret = "월";
+            else if (nowDt.DayOfWeek == DayOfWeek.Tuesday)
+                ret = "화";
+            else if (nowDt.DayOfWeek == DayOfWeek.Wednesday)
+                ret = "수";
+            else if (nowDt.DayOfWeek == DayOfWeek.Thursday)
+                ret = "목";
+            else if (nowDt.DayOfWeek == DayOfWeek.Friday)
+                ret = "금";
+            else if (nowDt.DayOfWeek == DayOfWeek.Saturday)
+                ret = "토";
+            else if (nowDt.DayOfWeek == DayOfWeek.Sunday)
+                ret = "일";
+
+            return ret;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff",
+            //                                CultureInfo.InvariantCulture);
+            DateTime nowDt = DateTime.Now;
+            string timestamp = nowDt.ToString("yyyy-MM-dd HH:mm:ss.fff",
+                                            CultureInfo.InvariantCulture);
+
+            label1.Text = GetWeek(nowDt) + "요일 " + timestamp;
+        }
+
+        // Copy this text into the Label using
+        // the biggest font that will fit.
+        private void SizeLabelFont(Label lbl)
+        {
+            // Only bother if there's text.
+            string txt = lbl.Text;
+            if (txt.Length > 0)
+            {
+                int best_size = 100;
+
+                // See how much room we have, allowing a bit
+                // for the Label's internal margin.
+                int wid = lbl.DisplayRectangle.Width - 10;
+                int hgt = lbl.DisplayRectangle.Height - 10;
+
+                // Make a Graphics object to measure the text.
+                using (Graphics gr = lbl.CreateGraphics())
+                {
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        using (Font test_font =
+                            new Font(lbl.Font.FontFamily, i))
+                        {
+                            // See how much space the text would
+                            // need, specifying a maximum width.
+                            SizeF text_size =
+                                gr.MeasureString(txt, test_font);
+                            if ((text_size.Width > wid) ||
+                                (text_size.Height > hgt))
+                            {
+                                best_size = i - 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Use that font size.
+                lbl.Font = new Font(lbl.Font.FontFamily, best_size);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double width = Screen.PrimaryScreen.Bounds.Width;
+            double height = Screen.PrimaryScreen.Bounds.Height;
+
+            this.Location = new Point(0, 0);
+            this.Width = (int)(width);
+            this.Height = (int)(height * 0.2);
+
+            SizeLabelFont(label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
